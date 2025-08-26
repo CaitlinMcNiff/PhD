c7b205ea2d44b5e8de954b64058afefd9dd3efb3
CaitlinMcNiff <s2506305@ed.ac.uk>
2025-08-26 14:55:56 +0100

    25/08/25: File that counts the number of different cCRE within the phenotype-associated population variants.

---
+++ scripts/cCRE_analysis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Output file
+output="/exports/cmvm/eddie/sbms/groups/young-lab/caitlin/phd/preliminary_exploration/cCRE/ccre_counts.txt"
+> "$output"  # Empty the file if it already exists
+echo -e "Population\tpromoter\tenhancer\ttotal_ccres" > "${output}"
+
+# Loop through all population files (edit pattern if needed)
+for file in /exports/cmvm/eddie/sbms/groups/young-lab/caitlin/phd/preliminary_exploration/cCRE/*_all_ccres.bed; do
+    # Extract population name (e.g., AFR from AFR_file.txt)
+    population=$(basename "$file" | cut -d'_' -f1)
+
+    # Count lines where the second-last column contains 'prom'
+    count_prom=$(awk -F'\t' '{ if ($(NF-1) ~ /prom/) ++c } END { print c+0 }' "$file")
+    count_enh=$(awk -F'\t' '{ if ($(NF-1) ~ /enh/) ++c } END { print c+0 }' "$file")
+    total_count=$(wc -l < "$file")
+
+    # Write result
+    echo -e "${population}\t${count_prom}\t${count_enh}\t${total_count}" >> "$output"
+done
